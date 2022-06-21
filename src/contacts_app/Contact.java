f70490ff8b75d1b638ee473a3076d01087fb0bf0
Justin English <justin.english39@gmail.com>
2022-06-21 10:51:14 -0500

    Feat : Menu Works as intended, Breaks when needed, no longer infinite loop

--- src/contacts_app/Contact.java
+++ src/contacts_app/Contact.java
@@ -5,37 +5,64 @@ import java.util.Scanner;
 public class Contact {
     public static void main(String[] args) {
 
-        System.out.println("1. View Saved Contacts");
-        System.out.println("2. Add New Contacts");
-        System.out.println("3. Search Saved contacts by name");
-        System.out.println("4. Delete Existing Contact");
-        System.out.println("5. Exit/Close Application");
-        System.out.println("Enter an option(1, 2, 3, 4, or 5): ");
 
-        Scanner sc = new Scanner(System.in);
-        int UserChoice = sc.nextInt();
+        boolean StopOn = true;
+        do {
+            System.out.println("1. View Saved Contacts");
+            System.out.println("2. Add New Contacts");
+            System.out.println("3. Search Saved contacts by name");
+            System.out.println("4. Delete Existing Contact");
+            System.out.println("5. Exit/Close Application");
+            System.out.println("Enter an option(1, 2, 3, 4, or 5): ");
 
-        if (UserChoice == 1){
-            //view contacts
-            System.out.println("1 was selected");
+            Scanner sc = new Scanner(System.in);
+            int UserChoice = sc.nextInt();
+            switch (UserChoice) {
+                case 1:
+                    System.out.println("was 1 selected");
+                    break;
+                case 2:
+                    System.out.println("was 2 selected");
+                    break;
+                case 3:
+                    System.out.println("was 3 selected");
+                    break;
+                case 4:
+                    System.out.println("was 4 selected");
+                    break;
+                case 5:
+                    System.out.println("was 5 selected");
+                    StopOn=false;
+                    break;
+            }
 
-        }
-        if (UserChoice == 2){
-            //add contacts
-            System.out.println("2 was selected");
-        }
-        if (UserChoice == 3){
-            //search for contacts
-            System.out.println("3 was selected");
-        }
-        if (UserChoice == 4){
-            //delete contacts
-            System.out.println("4 was selected");
-        }
-        if (UserChoice == 5){
-            //exit app
-            System.out.println("5 was selected");
-        }
+        } while (StopOn);
+
+    }
+}
+
+
+
+//        if (UserChoice == 1){
+//            //view contacts
+//            System.out.println("1 was selected");
+//        }
+//        if (UserChoice == 2){
+//            //add contacts
+//            System.out.println("2 was selected");
+//        }
+//        if (UserChoice == 3){
+//            //search for contacts
+//            System.out.println("3 was selected");
+//        }
+//        if (UserChoice == 4){
+//            //delete contacts
+//            System.out.println("4 was selected");
+//        }
+//        if (UserChoice == 5){
+//            //exit app
+//            System.out.println("5 was selected");
+//        }
 
 
 //        public class ContactsApp{
@@ -46,5 +73,5 @@ public class Contact {
 //
 //            public static void initializedFile()  {
 
-    }
-}
+//    }
+//}
