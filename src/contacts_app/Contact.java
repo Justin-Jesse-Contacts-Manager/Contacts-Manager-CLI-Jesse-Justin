67c14259f87fc6a75ef8d21085385b427f0312c5
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-06-21 11:55:36 -0400

    feat: contact class added

--- src/contacts_app/Contact.java
+++ src/contacts_app/Contact.java
@@ -1,77 +1,27 @@
 package contacts_app;
 
-import java.util.Scanner;
-
 public class Contact {
-    public static void main(String[] args) {
+    private String name;
+    private long number;
 
+    public Contact(String name, long number) {
+        this.name = name;
+        this.number = number;
+    }
 
-        boolean StopOn = true;
-        do {
-            System.out.println("1. View Saved Contacts");
-            System.out.println("2. Add New Contacts");
-            System.out.println("3. Search Saved contacts by name");
-            System.out.println("4. Delete Existing Contact");
-            System.out.println("5. Exit/Close Application");
-            System.out.println("Enter an option(1, 2, 3, 4, or 5): ");
+    public String getName() {
+        return name;
+    }
 
-            Scanner sc = new Scanner(System.in);
-            int UserChoice = sc.nextInt();
-            switch (UserChoice) {
-                case 1:
-                    System.out.println("was 1 selected");
-                    break;
-                case 2:
-                    System.out.println("was 2 selected");
-                    break;
-                case 3:
-                    System.out.println("was 3 selected");
-                    break;
-                case 4:
-                    System.out.println("was 4 selected");
-                    break;
-                case 5:
-                    System.out.println("was 5 selected");
-                    StopOn=false;
-                    break;
-            }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        } while (StopOn);
+    public long getNumber() {
+        return number;
+    }
 
+    public void setNumber(long number) {
+        this.number = number;
     }
 }
-
-
-
-//        if (UserChoice == 1){
-//            //view contacts
-//            System.out.println("1 was selected");
-//        }
-//        if (UserChoice == 2){
-//            //add contacts
-//            System.out.println("2 was selected");
-//        }
-//        if (UserChoice == 3){
-//            //search for contacts
-//            System.out.println("3 was selected");
-//        }
-//        if (UserChoice == 4){
-//            //delete contacts
-//            System.out.println("4 was selected");
-//        }
-//        if (UserChoice == 5){
-//            //exit app
-//            System.out.println("5 was selected");
-//        }
-
-
-//        public class ContactsApp{
-//            private static path filepath = path.of(System.getProperty("user.dir"));
-//            private static File file;
-//            private Static ArrayList<Contacts> contacts = new ArrayList<>();
-//
-//
-//            public static void initializedFile()  {
-
-//    }
-//}
